0ace82fbbe4e3117654e5a234edddae631825cc1
zadox473-lang <zadox473@gmail.com>
2026-05-28 15:29:18 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
+/* FIREBASE CONFIG */
 
+const firebaseConfig = {
+
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+
+};
+
+/* FIREBASE INIT LATER */
