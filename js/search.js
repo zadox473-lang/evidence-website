85bce3f0dd2bf30bde0b162268c11a222c3ad6c5
zadox473-lang <zadox473@gmail.com>
2026-05-28 15:26:11 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,21 @@
+const searchInput = document.querySelector(".search-section input");
 
+searchInput.addEventListener("keyup", () => {
+
+    const value = searchInput.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".report-card");
+
+    cards.forEach(card => {
+
+        const text = card.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            card.style.display = "block";
+        }else{
+            card.style.display = "none";
+        }
+
+    });
+
+});
