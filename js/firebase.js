116197fbca113a5ba974c284c1e54a2f36553ebe
zadox473-lang <zadox473@gmail.com>
2026-05-29 15:08:20 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,17 @@
-/* FIREBASE CONFIG */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-
+    apiKey: "AIzaSyCkMVWQ4NCqV7yJIUwBwu2NImetw7zUJeY",
+    authDomain: "scammer-database.firebaseapp.com",
+    projectId: "scammer-database",
+    storageBucket: "scammer-database.firebasestorage.app",
+    messagingSenderId: "257014278130",
+    appId: "1:257014278130:web:1c527b320f36b4d6c5ed6b"
 };
 
-/* FIREBASE INIT LATER */
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
